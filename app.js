5de4d37afff27632be4316eea9f6a4a6d2f90efa
milesfwilson <milesfwilson@gmail.com>
2020-10-10 16:54:49 -0600

    finished layout and functionality

--- app.js
+++ app.js
@@ -2,10 +2,49 @@ let apples = 0
 let statsBox = document.getElementById("stats")
 let inventoryBox = document.getElementById("inventory")
 let buttons = document.getElementById("buttons")
+let badges = document.getElementById("badges")
 let totalMod = 0
-let inflation = 1.5
+let inflation = 1.1
 let collectionInterval = 0
 let FPS = 0
+let badgesNum = 0
+let unlock = 0
+
+let badgeOptions = {
+  trophy: {
+    name: "Trophy",
+    applesNeeded: 100,
+    icon: `<i class="fa fa-trophy fa-3x" aria-hidden="true"></i>`
+  },
+  star: {
+    name: "Star",
+    applesNeeded: 1000,
+    icon: `<i class="fa fa-star fa-3x" aria-hidden="true"></i>`
+  },
+  apple: {
+    name: "Apple",
+    applesNeeded: 100000,
+    icon: `<i class="fas fa-apple-alt fa-3x"></i>`
+  },
+  award: {
+    name: "Award",
+    applesNeeded: 500000,
+    icon: `<i class="fas fa-award fa-3x"></i>`
+  },
+  wizard: {
+    name: "Wizard",
+    applesNeeded: 1000000,
+    icon: `<i class="fas fa-hat-wizard fa-3x"></i>`
+  },
+  tractor: {
+    name: "Tractor",
+    applesNeeded: 5000000,
+    icon: `<i class="fas fa-tractor fa-3x"></i>`
+  }
+
+}
+
+
 
 let player = {
   name: "Steve",
@@ -56,7 +95,8 @@ function startInterval() {
 }
 
 function collectAutoUpgrades() {
-  console.log(apples += FPS)
+  apples += FPS
+  unlock += FPS
   update()
 }
 
@@ -83,8 +123,10 @@ function pick() {
 
   if (totalMod > 0) {
     apples += (1 * totalMod)
+    unlock += (1 * totalMod)
   } else {
     ++apples
+    ++unlock
   }
 
   upgradeAdd()
@@ -102,27 +144,34 @@ function drawButtons() {
 
       template += `
       <div class="col-3 text-center">
+      <div class="bg-light shadow radius-25 p-3">
       <h5>${upgrades[key].name}</h5>
-
-      <div class="d-flex my-2 mx-auto">
-      <i class="fa fa-plus-circle fa-2x" aria-hidden="true"></i>
-      <h5 class="ml-2 my-auto">${upgrades[key].multiplier}</h5>
+      
+      <div class="d-flex my-2 justify-content-center">
+      <h6 class="mr-2 my-auto">${upgrades[key].multiplier}</h6>
+      <i class="fas fa-times mt-1"></i>
       </div>
-
-
-      <div class="d-flex my-2 mx-auto">
-      <i class="fa fa-money fa-2x" aria-hidden="true"></i>
-      <h5 class="ml-2 my-auto">${Math.floor(upgrades[key].price)}</h5>
-      </div>`
+      
+      
+      `
 
       if (apples >= upgrades[key].price) {
         template +=
-          `<button class="btn btn-success shadow" onclick="addMods('${key}')">Buy</button>
-  </div>
-  `
-      } else {
-        template += `<button class="btn btn-primary shadow" onclick="addMods('${key}')" disabled>Buy</button>
+          `<button class="btn btn-success btn-block shadow" onclick="addMods('${key}')"><div class="d-flex justify-content-center">
+        <i class="fas fa-coins text-warning my-2"></i>
+        <h6 class="ml-auto my-auto">${Math.floor(upgrades[key].price)}</h6>
+        </div></button>
         </div>
+        </div>
+        `
+      } else {
+        template += `<button class="btn btn-primary shadow btn-block" onclick="addMods('${key}')" disabled><div class="d-flex justify-content-center">
+        <i class="fas fa-coins text-warning my-2"></i>
+        <h6 class="ml-auto my-auto">${Math.floor(upgrades[key].price)}</h6>
+        </div></button>
+        </div>
+        </div>
+
         `
       }
     }
@@ -130,32 +179,55 @@ function drawButtons() {
   buttons.innerHTML = template
 }
 
+function drawBadges() {
+
+
+  let template = ""
+  for (const key in badgeOptions) {
+    if (badgeOptions.hasOwnProperty(key)) {
+
+      if (unlock > badgeOptions[key]['applesNeeded']) {
+
+        template += `<div class="p-2 text-warning text-center">
+        ${badgeOptions[key]['icon']}
+        <h6 class="mt-1">${badgeOptions[key]['applesNeeded']}</h6>
+        </div>
+        `
+      }
+
+    }
+  }
+
+  badges.innerHTML = template
+}
+
 function update() {
+
   statsBox.innerHTML = `
-  <div class="d-flex justify-content-around">
-  <h5>Stats</h5> 
-  <i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>
+  <div class="d-flex justify-content-center text-center bg-dark radius-25 shadow py-2 px-3 text-light">
+  <i class="far fa-chart-bar fa-2x mr-2"></i>
+  <h5 class="my-auto">Stats</h5> 
   </div>
-          <div class="text-left">
+          <div class="text-left shadow bg-light radius-25 p-3 mt-3 text-dark">
           <h6>Apples: ${Math.floor(apples)}</h6>
-          <h6>FPS: ${FPS}</h6>
-          <h6>Multiplier: ${totalMod}</h6>
+          <h6>APS: ${FPS}</h6>
+          <h6>Hands: ${totalMod}</h6>
           </div>
   `
   inventoryBox.innerHTML = `
-  <div class="d-flex justify-content-around">
-  <h5>Inventory</h5> 
-  <i class="fa fa-info fa-2x" aria-hidden="true"></i>
+  <div class="d-flex justify-content-center text-center bg-dark radius-25 shadow py-2 px-3 text-light">
+  <i class="fas fa-truck-moving fa-2x mr-2"></i>
+  <h5 class="my-auto">Inventory</h5> 
 
   </div>
-          <div class="text-left">
+          <div class="text-left bg-light shadow radius-25 p-3 mt-3 text-dark">
           <h6>Baskets: ${upgrades.basket.quantity}</h6>
           <h6>Ladders: ${upgrades.ladder.quantity}</h6>
           <h6>Work Crews: ${upgrades.workCrew.quantity}</h6>
           <h6>Tractors: ${upgrades.tractor.quantity}</h6>
           </div>
 `
-
+  drawBadges()
   drawButtons()
 }
 
